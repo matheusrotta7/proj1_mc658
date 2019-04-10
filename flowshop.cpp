a3979d365322360a0030a781e1d43fc66d875126
gpspelle <gpsunicamp016@gmail.com>
2019-04-10 20:26:43 -0300

    [working, cuts with optimality, dual bound. s1] need to implement s2 and heap

--- flowshop.cpp
+++ flowshop.cpp
@@ -1,14 +1,8 @@
 #include<bits/stdc++.h>
 
-using namespace std;
-
-/* https://hal.archives-ouvertes.fr/hal-00680452v1/document
- *
- *
- * */
-
-//#define INF numeric_limits<float>::infinity();
 #define INF numeric_limits<int>::max();
+
+using namespace std;
 
 typedef struct job {
     int d1; //duração da tarefa na máquina 1
@@ -34,6 +28,17 @@ struct Comp {
     vector<job> paramA;
 };
 
+
+int s1(node &cur_node, vector<job> &jobs, int n);
+int calc_bound(node &cur_node, vector<job> jobs, int n);
+long long int calc_end_time_m2(vector<job> &jobs, vector<int> &chosen_jobs);
+long long int branch(long long int limitante_primal, vector<node> &active_nodes, int min_pos, int n, vector<job> jobs);
+long long int bnb(vector<job> jobs, int n, vector<node> active_nodes);
+
+/* https://hal.archives-ouvertes.fr/hal-00680452v1/document
+ *
+ *
+ * */
 
 /* relaxação 1: overlapping permitido na máquina 2
  * cada job começa logo após o seu término na máquina 1
@@ -63,22 +68,82 @@ struct Comp {
      return sum;
  }
 
-int s2(node &n, vector<job> &jobs, int n) {
+/*int s2(node &n, vector<job> &jobs, int n) {
+
+}*/
+
+int calc_bound(node &cur_node, vector<job> jobs, int n) {
+    int chosen_acc = calc_end_time_m2(jobs, cur_node.chosen_jobs);
+    return cur_node.classif = chosen_acc + s1(cur_node, jobs, n);
+
+    /*
+ *    SO FAR WE DO NOT HAVE S1
+ * */
+    //return cur_node.classif = chosen_acc + max(s1(cur_node, jobs), s2(cur_node, jobs));
+}
+
+long long int calc_end_time_m2(vector<job> &jobs, vector<int> &chosen_jobs) {
+
+    /* vector containing endtime for machine 1 (m1) */ 
+    vector<int>  end_time_1; 
+
+    /* adding dummy node to calculate end_time for m1 */
+    end_time_1.push_back(0);
+    
+    /* vector containing endtime for machine 2 (m2) */ 
+    vector<int>  end_time_2; 
+
+    /* adding dummy node to calculate end_time for m2 */
+    end_time_2.push_back(0);
+
+    /* get number of chosen jobs for a leaf */
+    int num_of_jobs = chosen_jobs.size();
+
+    /* create a vector with end times (m1) for each chosen job in its order */
+    for(int i = 0; i < num_of_jobs; i++) { 
+        end_time_1.push_back(jobs[chosen_jobs[i]].d1 + end_time_1.back());
+    }
+
+    /* removing dummy node used to easilly calculate above function */
+    end_time_1.erase(end_time_1.begin());
+
+    long long int sum = 0;
+    for(int i = 0; i < num_of_jobs; i++) {
+        if(end_time_1[i] >= end_time_2.back()) {
+            /* enter here menas that machine 2 is ready to operate now */
+            end_time_2.push_back(end_time_1[i] + jobs[chosen_jobs[i]].d2);
+        } else {
+            end_time_2.push_back(end_time_2.back() + jobs[chosen_jobs[i]].d2); 
+        }
+        sum += end_time_2.back();
+    } 
+
+    // This erase is only necessary if you want to check end_time_2 values
+    //end_time_2.erase(end_time_2.begin());
+    return sum;
 
 }
 
-int calc_bound(&node cur_node, vector<job> jobs, int n) {
-    int chosen_acc = (-----);
-    return cur_node.classif = chosen_acc + max(s1(cur_node, jobs), s2(cur_node, jobs));
-}
-
-long long int branch (vector<node> &active_nodes, int min_pos, int n) {
+long long int branch(long long int limitante_primal, vector<node> &active_nodes, int min_pos, int n, vector<job> jobs) {
     int num_of_jobs = active_nodes[min_pos].jobs_in_m.size();
     if (num_of_jobs == 0) {
         long long int sum = 0;
 
         //calcular para a folha
         //(já está tudo definido)
+        /*
+ *         TODO: project decision: eliminate all the active nodes with classif
+ *         higher than limitante_primal calculated everytime we get here. 
+ *         Or only remove active nodes when limitante_primal is updated with
+ *         a smaller value.
+ *
+ *         Taken decision: only eliminate active nodes when limitante_primal is
+ *         updated, because it means we found a good new value. If it wasnt't
+ *         update we may not cut many active nodes and cut nodes that will take
+ *         longer to be open (wouldnt even be open maybe).
+ *      */
+        cout << "FOLHAAAAA\n";
+        return calc_end_time_m2(jobs, active_nodes[min_pos].chosen_jobs); 
 
     }
     else {
@@ -88,18 +153,32 @@ long long int branch (vector<node> &active_nodes, int min_pos, int n) {
             cur_job = active_nodes[min_pos].jobs_in_m[i];
             node new_n;
             for (int j = 0; j < num_of_jobs; j++) {
-                if (j != cur_job) {
-                    new_n.jobs_in_m.push_back(j);
+                if (active_nodes[min_pos].jobs_in_m[j] != cur_job) {
+                    new_n.jobs_in_m.push_back(active_nodes[min_pos].jobs_in_m[j]);
                 }
             }
 
             int num_of_chosen = active_nodes[min_pos].chosen_jobs.size();
             for(int j = 0; j < num_of_chosen; j++) {
-                new_n.chosen_jobs.push_back(j);
+                new_n.chosen_jobs.push_back(active_nodes[min_pos].chosen_jobs[j]); 
             }
 
             new_n.chosen_jobs.push_back(cur_job);
             new_n.classif = calc_bound(new_n, jobs, n);
+
+            /* if we already have something equal or better than this node is
+            * promessing, we do not need to consider it */
+            /*
+ *             TODO: a possible optimization is to find fast a limitante_primal
+ *             so whenever you try to open a new node you check if that node
+ *             deserves to be inserted. A better optimization is to find fast
+ *             a really low limitante_primal to this problem of minimization. 
+ *      */
+
+            if(new_n.classif >= limitante_primal) {
+                continue;
+            }
+
             active_nodes.push_back(new_n);
 
         }
@@ -109,7 +188,6 @@ long long int branch (vector<node> &active_nodes, int min_pos, int n) {
 
 }
 
-
 long long int bnb(vector<job> jobs, int n, vector<node> active_nodes) {
 
     //temos duas divisões em conjuntos:
@@ -118,7 +196,6 @@ long long int bnb(vector<job> jobs, int n, vector<node> active_nodes) {
     //jobs em M e jobs fora de M (em M já decididos, fora ainda a explorar)
 
     long long int limitante_primal = INF;
-
 
     /*  TODO: esse while pode ter outra condição ne? a gente nao precisa ficar abrindo
      *  mais nos depois que limitante_primal = limitante_dual
@@ -135,8 +212,23 @@ long long int bnb(vector<job> jobs, int n, vector<node> active_nodes) {
             }
         }
 
-        limitante_primal = min(limitante_primal, branch (active_nodes, min_pos, n));
-        active_nodes.erase(active_nodes.begin() + min_pos); //pop explored node
+        long long int old_limitante_primal = limitante_primal;
+        limitante_primal = min(limitante_primal, branch(limitante_primal, active_nodes, min_pos, n, jobs));
+        active_nodes.erase(active_nodes.begin() + min_pos);
+
+        /* Check if limitante_primal was updated by above min() */
+        /*
+ *         TODO: the below procedure gets REALLY faster using PriorityQueue
+ *      */
+        if (old_limitante_primal != limitante_primal) {
+            an_size -= 1;
+            for(int i = 0; i < an_size; i++) {
+                if(limitante_primal <= active_nodes[i].classif) {
+                    active_nodes.erase(active_nodes.begin() + i);
+                }
+            }
+
+        }
     }
 
     return limitante_primal;
@@ -161,13 +253,11 @@ int main() {
     for (int i = 0; i < n; i++) {
         initial.jobs_in_m.push_back(i);
     }
-    initial.classif = calc_bound(initial, jobs);
+    initial.classif = calc_bound(initial, jobs, n);
     active_nodes.push_back(initial);
     /*a gente tem que implementar um algoritmo bruteforce que faz bfs no
     espaço de busca e faz bound pra melhor opção dada a função classificadora*/
     long long int sft; //sft := sum of finishing times (m2)
-
-
 
     sft = bnb(jobs, n, active_nodes);
     cout << sft << '\n';
